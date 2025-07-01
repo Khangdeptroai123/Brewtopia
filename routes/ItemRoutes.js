d674d756c73af726b6d3451566704d7432c63280
Khangdeptroai123 <khangdpse172560@fpt.edu.vn>
2025-07-02 00:49:25 +0700

    okay

--- routes/ItemRoutes.js
+++ routes/ItemRoutes.js
@@ -1,30 +1,30 @@
 const express = require("express");
 const {
-  addItem,
-  updateItem,
-  deleteItem,
+  // addItem,
+  // updateItem,
+  // deleteItem,
   getItems,
 } = require("../controllers/itemController");
-const { authenticateUser } = require("../middlewares/authMiddleware");
-const { authorizeRoles } = require("../middlewares/roleMiddleware");
-const { uploadFields } = require("../middlewares/upload");
+// const { authenticateUser } = require("../middlewares/authMiddleware");
+// const { authorizeRoles } = require("../middlewares/roleMiddleware");
+// const { uploadFields } = require("../middlewares/upload");
 
 const router = express.Router();
 
 router.get("/:menuId", getItems);
 // Thêm món Item
-router.post(
-  "/create-Item/:id",
-  authenticateUser,
-  authorizeRoles(["admin"]),
-  uploadFields("itemMenu-images", [{ name: "image", maxCount: 1 }]),
-  addItem
-);
+// router.post(
+//   "/create-Item/:id",
+//   authenticateUser,
+//   authorizeRoles(["admin"]),
+//   uploadFields("itemMenu-images", [{ name: "image", maxCount: 1 }]),
+//   addItem
+// );
 
-// Cập nhật Item
-router.put("/:id", authenticateUser, authorizeRoles(["admin"]), updateItem);
+// // Cập nhật Item
+// router.put("/:id", authenticateUser, authorizeRoles(["admin"]), updateItem);
 
-// Xoá Item
-router.delete("/:id", authenticateUser, authorizeRoles(["admin"]), deleteItem);
+// // Xoá Item
+// router.delete("/:id", authenticateUser, authorizeRoles(["admin"]), deleteItem);
 
 module.exports = router;
